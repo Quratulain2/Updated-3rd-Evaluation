30e57bc4f81bdfe5ea374881f8ffea3814e25332
Quratulain2 <49489075+Quratulain2@users.noreply.github.com>
2021-08-01 22:43:46 +0500

    Update Dept.js

--- components/Dept.js
+++ components/Dept.js
@@ -13,14 +13,6 @@ class Dept extends Component{
 
  render(){
 
-  function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
   
     return(
         <div className="content">
